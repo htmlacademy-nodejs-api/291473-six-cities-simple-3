36cf7a335b277ac5cdc186a6c0a158f99ce58fed
Дмитрий Кречетников <dkrech07@gmail.com>
2022-12-26 16:12:51 +0500

    Начинает добавлять набор тестовых данных

---
+++ src/types/mock-data.type.ts
@@ -0,0 +1,10 @@
+export type MockData = {
+  titles: string[];
+  descriptions: string[];
+  cities: string[];
+  previewImages: string[];
+  detailImages: string[];
+  users: string[];
+  emails: string[];
+  avatars: string[];
+};
