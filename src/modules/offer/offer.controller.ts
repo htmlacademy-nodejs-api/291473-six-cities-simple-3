8ddec0e3ce1b5812add6b81e8854db3bb79b64f8
Дмитрий <dkrech07@gmail.com>
2023-01-30 01:18:20 +0500

    Реализует кейсы по ТЗ

--- src/modules/offer/offer.controller.ts
+++ src/modules/offer/offer.controller.ts
@@ -9,7 +9,7 @@ import { StatusCodes } from 'http-status-codes';
 import OfferResponse from './response/offer.response.js';
 import { fillDTO } from '../../utils/common.js';
 import CreateOfferDto from './dto/create-offer.dto.js';
-// import HttpError from '../../common/errors/http-error.js';
+import UpdateOfferDto from './dto/update-offer.dto.js';
 
 @injectable()
 export default class OfferController extends Controller {
@@ -23,11 +23,11 @@ export default class OfferController extends Controller {
 
     this.addRoute({ path: '/', method: HttpMethod.Get, handler: this.index });
     this.addRoute({ path: '/', method: HttpMethod.Post, handler: this.create });
-  }
+    this.addRoute({ path: '/:offerId', method: HttpMethod.Post, handler: this.updateById });
+    this.addRoute({ path: '/:offerId', method: HttpMethod.Delete, handler: this.deleteById });
+    this.addRoute({ path: '/:offerId', method: HttpMethod.Get, handler: this.findById });
 
-  //   index - получить;
-  //   create - создать;
-  //   delete - удалить;
+  }
 
   public async index(_req: Request, res: Response): Promise<void> {
     const offers = await this.offerService.find();
@@ -39,22 +39,35 @@ export default class OfferController extends Controller {
     { body }: Request<Record<string, unknown>, Record<string, unknown>, CreateOfferDto>,
     res: Response): Promise<void> {
 
-    // const existOffer = await this.offerService.findByOfferName(body.name);
-
-    // if (existOffer) {
-    //   throw new HttpError(
-    //     StatusCodes.UNPROCESSABLE_ENTITY,
-    //     `Offer with name «${body.name}» exists.`,
-    //     'OfferController'
-    //   );
-    // }
-
     const result = await this.offerService.create(body);
     this.send(
       res,
       StatusCodes.CREATED,
       fillDTO(OfferResponse, result)
     );
+  }
+
+  public async updateById({ params, body }: Request<Record<string, unknown>, Record<string, unknown>, UpdateOfferDto>,
+    res: Response): Promise<void> {
+
+    const result = await this.offerService.updateById(params.offerId as string, body);
+    this.send(
+      res,
+      StatusCodes.CREATED,
+      fillDTO(OfferResponse, result)
+    );
+  }
+
+  public async deleteById(req: Request, res: Response): Promise<void> {
+    await this.offerService.deleteById(req.params.offerId as string);
+    this.send(res, StatusCodes.OK, null);
+  }
+
+  public async findById(req: Request, res: Response): Promise<void> {
+
+    const offer = await this.offerService.findById(req.params.offerId);
+    const offerResponse = fillDTO(OfferResponse, offer);
+    this.send(res, StatusCodes.OK, offerResponse);
   }
 }
 
