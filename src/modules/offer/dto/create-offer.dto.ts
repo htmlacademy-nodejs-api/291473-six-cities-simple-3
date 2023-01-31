22f9b05eee72d4365c8395a62b211a7c05a623c1
Дмитрий Кречетников <dkrech07@gmail.com>
2023-01-31 17:01:24 +0500

    Начинает добавлять валидаторы для CreateOfferDto

--- src/modules/offer/dto/create-offer.dto.ts
+++ src/modules/offer/dto/create-offer.dto.ts
@@ -1,10 +1,19 @@
 import { Coordinates } from '../../../types/coordinates.type.js';
 import { housingType } from '../../../types/housing-type.enum.js';
+import { IsArray, IsDateString, IsEnum, IsInt, IsMongoId, Max, MaxLength, Min, MinLength } from 'class-validator';
 
 export default class CreateOfferDto {
+  @MinLength(10, { message: 'Minimum title length must be 10' })
+  @MaxLength(100, { message: 'Maximum title length must be 100' })
   public title!: string;
+
+  @MinLength(20, { message: 'Minimum description length must be 20' })
+  @MaxLength(1024, { message: 'Maximum description length must be 1024' })
   public description!: string;
+
+  @IsDateString({}, { message: 'postDate must be valid ISO date' })
   public postDate!: Date;
+
   public city!: string;
   public previewImagePath!: string;
   public detailImagePath!: string[];
@@ -15,7 +24,12 @@ export default class CreateOfferDto {
   public housingType!: housingType;
   public roomsNumber!: number;
   public guestsNuber!: number;
+
+  @IsInt({ message: 'Price must be an integer' })
+  @Min(100, { message: 'Minimum price is 100' })
+  @Max(100000, { message: 'Maximum price is 100000' })
   public rentPrice!: number;
+
   public amenities!: string[];
   public commentsCount!: number;
   public coordinates!: Coordinates;
