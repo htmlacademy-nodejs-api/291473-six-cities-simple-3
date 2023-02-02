8abe8baeeec8f37a7abd390b69a49b114a6ce7f5
Дмитрий Кречетников <dkrech07@gmail.com>
2023-02-02 13:42:24 +0500

    Добавляет поиск пользователя (verifyUser), проверку пароля (verifyPassword), выносит константы в enum

---
+++ src/modules/user/user.constant.ts
@@ -0,0 +1,4 @@
+export enum UserDefaults {
+  usersCount = 60,
+  jwtAlgoritm = 'HS256',
+}
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -6,6 +6,8 @@ import { UserServiceInterface } from './user-service.interface.js';
 import { LoggerInterface } from '../../common/logger/logger.interface.js';
 import { Component } from '../../types/component.types.js';
 import UpdateUserDto from './dto/update-user.dto.js';
+import LoginUserDto from './dto/login-user.dto.js';
+import { UserDefaults } from './user.constant.js';
 
 @injectable()
 export default class UserService implements UserServiceInterface {
@@ -27,7 +29,7 @@ export default class UserService implements UserServiceInterface {
   public async find(): Promise<DocumentType<UserEntity>[]> {
     return this.userModel
       .find()
-      .limit(60)
+      .limit(UserDefaults.usersCount)
       .exec();
   }
 
@@ -49,5 +51,19 @@ export default class UserService implements UserServiceInterface {
     return this.userModel
       .findByIdAndUpdate(userId, dto, { new: true })
       .exec();
+  }
+
+  public async verifyUser(dto: LoginUserDto, salt: string): Promise<DocumentType<UserEntity> | null> {
+    const user = await this.findByEmail(dto.email);
+
+    if (!user) {
+      return null;
+    }
+
+    if (user.verifyPassword(dto.password, salt)) {
+      return user;
+    }
+
+    return null;
   }
 }
