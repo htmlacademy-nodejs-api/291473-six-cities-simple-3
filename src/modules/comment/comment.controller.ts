9ee56de154794779134c36764ead3f5b8499a27d
Дмитрий Кречетников <dkrech07@gmail.com>
2023-02-02 16:10:15 +0500

    Добавляет автоматическое заполнение userId автора комментария (AuthenticateMiddleware)

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -33,10 +33,11 @@ export default class CommentController extends Controller {
   }
 
   public async create(
-    { body }: Request<object, object, CreateCommentDto>,
+    req: Request<object, object, CreateCommentDto>,
     res: Response
   ): Promise<void> {
 
+    const { body } = req;
     if (!await this.offerService.exists(body.offerId)) {
       throw new HttpError(
         StatusCodes.NOT_FOUND,
@@ -45,7 +46,7 @@ export default class CommentController extends Controller {
       );
     }
 
-    const comment = await this.commentService.create(body);
+    const comment = await this.commentService.create({ ...body, userId: req.user.id });
     await this.offerService.incCommentCount(body.offerId);
     this.created(res, fillDTO(CommentResponse, comment));
   }
--- src/modules/comment/dto/create-comment.dto.ts
+++ src/modules/comment/dto/create-comment.dto.ts
@@ -22,6 +22,5 @@ export default class CreateCommentDto {
   @IsMongoId({ message: 'offerId field must be a valid id' })
   public offerId!: string;
 
-  @IsMongoId({ message: 'userId field must be a valid id' })
   public userId!: string;
 }
