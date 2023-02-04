a9c1fdd9ef92f54f7a9b11e841070d398ac5dac5
Дмитрий <dkrech07@gmail.com>
2023-02-04 23:22:13 +0500

    Добавляет ValidationError

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,8 +1,10 @@
 import crypto from 'crypto';
-import { plainToInstance } from 'class-transformer';
-import { ClassConstructor } from 'class-transformer/types/interfaces/class-constructor.type.js';
+import { plainToInstance, ClassConstructor } from 'class-transformer';
+import { ValidationError } from 'class-validator';
 import { Offer } from '../types/offer.type.js';
 import * as jose from 'jose';
+import { ValidationErrorField } from '../types/validation-error-field.type.js';
+import { ServiceError } from '../types/service-error.enum.js';
 
 export const createOffer = (row: string) => {
   const tokens = row.replace('\n', '').split('\t');
@@ -92,8 +94,10 @@ export const getNewRating = (overallRating: number, ratingCount: number, newRati
 export const fillDTO = <T, V>(someDto: ClassConstructor<T>, plainObject: V) =>
   plainToInstance(someDto, plainObject, { excludeExtraneousValues: true });
 
-export const createErrorObject = (message: string) => ({
-  error: message,
+export const createErrorObject = (serviceError: ServiceError, message: string, details: ValidationErrorField[] = []) => ({
+  errorType: serviceError,
+  message,
+  details: [...details]
 });
 
 export const createJWT = async (algoritm: string, jwtSecret: string, payload: object): Promise<string> =>
@@ -102,3 +106,10 @@ export const createJWT = async (algoritm: string, jwtSecret: string, payload: ob
     .setIssuedAt()
     .setExpirationTime('2d')
     .sign(crypto.createSecretKey(jwtSecret, 'utf-8'));
+
+export const transformErrors = (errors: ValidationError[]): ValidationErrorField[] =>
+  errors.map(({ property, value, constraints }) => ({
+    property,
+    value,
+    messages: constraints ? Object.values(constraints) : []
+  }));
