2553b717137f65e6dffea6a2e42462958f97e076
Дмитрий <dkrech07@gmail.com>
2023-02-05 22:53:10 +0500

    Подключает cors

--- src/app/application.ts
+++ src/app/application.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from 'inversify';
 import express, { Express } from 'express';
+import cors from 'cors';
 import { LoggerInterface } from '../common/logger/logger.interface.js';
 import { ConfigInterface } from '../common/config/config.interface.js';
 import { Component } from '../types/component.types.js';
@@ -45,6 +46,7 @@ export default class Application {
 
     const authenticateMiddleware = new AuthenticateMiddleware(this.config.get('JWT_SECRET'));
     this.expressApp.use(authenticateMiddleware.execute.bind(authenticateMiddleware));
+    this.expressApp.use(cors());
   }
 
   public initExceptionFilters() {
